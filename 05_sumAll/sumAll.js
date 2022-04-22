df64e2fe9d23b157f852f8fd2e11fe77a51446b8
gomeschian <celusher@g.ucla.edu>
2022-04-21 19:31:01 -0700

    Add solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+   if (typeof num1 != "number" || typeof num2 != "number")
+   {
+       return "ERROR";
+   }
+    else if (num1 > 0 && num2 > 0) 
+    {
+       
+    
+        let sum = 0;
+        let biggerNumber = 0;
+        let smallerNumber = 0;
+        
+        if (num1 > num2)
+        {
+            biggerNumber = num1;
+            smallerNumber = num2;
+        } 
+        else 
+        {
+            biggerNumber = num2;
+            smallerNumber = num1;
+        }        
+        for (let i = biggerNumber; i>= smallerNumber; i--)
+        {
+            sum += i;
+        }
+        return sum;
+    }
 
+    else return "ERROR";
 };
 
 // Do not edit below this line
